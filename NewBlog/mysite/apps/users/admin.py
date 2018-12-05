d475587fdd9e111961bbfa56666255d38cfdc056
tminlun <1272443075@qq.com>
2018-12-05 22:54:04 +0800

    Django进阶加油 2018/12/5

--- NewBlog/mysite/apps/users/admin.py
+++ NewBlog/mysite/apps/users/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile
+
 # Register your models here.
 
-@admin.register(UserProfile)
-class UserProfileAamin(admin.ModelAdmin):
-    list_display = ('nick_name', 'brithday', 'gander')
+
 
